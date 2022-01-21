eaee1ac7d8038f1f5014fe01a229fb0cb92388ce
mian <mianbilalsadiqpunnuhan@gmail.com>
2022-01-21 19:33:39 +0500

    added error page+other functionality

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 const path = require('path')
-
+const hbs = require('hbs')
 const express = require('express')
+
+
 const app = express()
 
 const port = process.env.PORT || 3000
@@ -8,7 +10,42 @@ const port = process.env.PORT || 3000
 //paths :
 //Public for static
 const publicDirectory = path.join(__dirname, '../public-directory')
+const viewsPath = path.join(__dirname, "../template/views")
+
+//view engine
 app.use(express.static(publicDirectory))
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+// for sending data for static pages:
+
+// app.get('/*', (req, res)=>{
+//     res.send({
+//         errorCode : "404",
+//         message : "Connection not estblished"
+//     })
+// })
+
+app.get('', (req,res)=>{
+    res.render('index',{
+        test: true
+    })
+})
+
+app.get('*', (req,res)=>{
+    res.render('errorPage',{
+        test: true
+    })
+})
+
+
+app.get('/Projects/*', (req,res)=>{
+    res.render('errorPage',{
+        test: true
+    })
+})
+
+
+
 
 app.listen(port, ()=>{
     console.log('Server is Listening')
